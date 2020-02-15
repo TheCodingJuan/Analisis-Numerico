e9e3cb17df2d6b5a36a4641e8f4c8a2968d57e92
Juan Camacho <53156142+TheCodingJuan@users.noreply.github.com>
2020-02-14 22:52:46 -0500

    Create epsilon_de_mi_maquina.py
    Epsilons

---
+++ Talleres/Taller 1/epsilon_de_mi_maquina.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 08:40:15 2020
+
+@author: Gabriela Camacho y Juan Camacho
+"""
+
+import numpy as np
+print(np.finfo(np.float64).eps) #Doble precision
+
+print(np.finfo(np.float32).eps) #Precision simple
